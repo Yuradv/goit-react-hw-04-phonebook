7dadfac3734b3b1ac68e7d1adf4f274c4929fba1
Yura Dolzhnikov <yuradolzhnikov@gmail.com>
2022-04-01 16:48:36 +0300

    Added localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,28 @@ import Notiflix from 'notiflix';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: [],
     filter: "",
+  };
+  
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  
+  
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }  
   };
   
   saveContact = ({ name, number }) => {
@@ -58,13 +73,14 @@ class App extends Component {
   };
 
   render() {
+    const {filter} = this.state
     return (
       <Container>
         <h1>Phonebook</h1>
         <Form onSubmit={this.saveContact} />
 
         <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.onChangeFilter }/>
+        <Filter value={filter} onChange={this.onChangeFilter} />
         <Contacts
           contacts={this.getVisibleContacts()}
           onDeleteContact={this.deleteContact }/>
@@ -75,3 +91,8 @@ class App extends Component {
 
 
 export default App;
+
+
+// if(this.state.contacts === []) {
+//           <h3>There are no contacts here, add something above</h3>
+//         }
